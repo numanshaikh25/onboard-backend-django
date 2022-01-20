533a47ee4099a5ebaa7dac7b1ed41aa3a7287abb
Numan Shaikh <86288396+numanshaikh25@users.noreply.github.com>
2022-01-20 17:40:20 +0530

    Update settings.py

--- onboard/settings.py
+++ onboard/settings.py
@@ -168,5 +168,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER='testcrinitis@gmail.com'
-EMAIL_HOST_PASSWORD='cbmunwumrypewhjz'
+EMAIL_HOST_USER='*'
+EMAIL_HOST_PASSWORD='*'
